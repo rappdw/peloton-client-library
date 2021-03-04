59ef46137e65023de6f6f5a3384bd80dd560ab0a
Dan Rapp <rappdw@gmail.com>
2021-03-04 10:58:07 -0700

    Return html

--- docker/server/app.py
+++ docker/server/app.py
@@ -8,10 +8,13 @@ app = Flask(__name__)
 def get_personal_stats():
     refresh()
     analysis = Analysis()
-    return f'You have exercised {analysis.accumulated_minutes:,} minutes so far this year.\n'\
-           f'We estimate you will complete {int(analysis.eoy_estimate):,} minutes this year.\n'\
-           f'Current daily streak is {analysis.current_daily_streak:,} days.\n'\
-           f'Current weekly streak is {analysis.current_weekly_streak:,} weeks.\n'
+    return f'<!DOCTYPE html>' \
+           f'<html>' \
+           f'<body>You have exercised {analysis.accumulated_minutes:,} minutes so far this year.<br/>'\
+           f'We estimate you will complete {int(analysis.eoy_estimate):,} minutes this year.<br/><br/>'\
+           f'Current daily streak is {analysis.current_daily_streak:,} days.<br/>'\
+           f'Current weekly streak is {analysis.current_weekly_streak:,} weeks.<br/>'\
+            '</body></html>'
 
 
 if __name__ == '__main__':
